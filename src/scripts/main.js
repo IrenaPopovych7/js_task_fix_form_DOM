cfdc0e6267f1f207b4e893d70cf642d0aab1329a
Iryna Popovych <popovicirina40@gmail.com>
2024-10-22 22:38:28 +0100

    capitalize first letter

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,10 +3,15 @@
 // write code here
 const allInputs = document.getElementsByTagName('input');
 
+function capitalizeFirstLetterOfString(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
 [...allInputs].forEach((input) => {
   const label = document.createElement('label');
+  const placeholderName = capitalizeFirstLetterOfString(input.name.split(/(?=[A-Z])/).join(' '));
 
-  input.setAttribute('placeholder', input.name.split(/(?=[A-Z])/).join(' '));
+  input.setAttribute('placeholder', placeholderName);
 
   label.setAttribute('for', input.id);
   label.className = 'field-label';
